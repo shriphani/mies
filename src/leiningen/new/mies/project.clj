f0838581326c5c4ce10e641a7fc7629c670bfab1
Shriphani Palakodety <shriphanip@gmail.com>
2015-07-16 13:43:31 -0700

    sets up mies with whatever is needed to use lein cljsbuild

--- src/leiningen/new/mies/project.clj
+++ src/leiningen/new/mies/project.clj
@@ -2,12 +2,28 @@
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "0.0-3308" :classifier "aot"
-                  :exclusion [org.clojure/data.json]]
+                 [org.clojure/clojurescript "0.0-2755" :exclusion [org.clojure/data.json]]
                  [org.clojure/data.json "0.2.6" :classifier "aot"]]
   :jvm-opts ^:replace ["-Xmx1g" "-server"]
   :node-dependencies [[source-map-support "0.3.2"]]
   :plugins [[lein-npm "0.5.0"]]
   :source-paths ["src" "target/classes"]
   :clean-targets ["out" "release"]
-  :target-path "target")
+  :cljsbuild {
+              :builds [{:id "dev"
+                        :source-paths ["src"]
+                        :compiler {
+                                   :main {{name}}.core
+                                   :output-to "out/{{name}}.js"
+                                   :output-dir "out"
+                                   :optimizations :none
+                                   :cache-analysis true
+                                   :source-map true}}
+                       {:id "release"
+                        :source-paths ["src"]
+                        :compiler {
+                                   :main {{name}}.core
+                                   :output-to "out-adv/{{name}}.min.js"
+                                   :output-dir "out-adv"
+                                   :optimizations :advanced
+                                   :pretty-print false}}]})
